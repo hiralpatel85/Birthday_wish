901ded1a94bd24069c1296ed46b6f8f81c280550
hiralpatel85 <hirpatel2314@gmail.com>
2023-07-26 11:44:17 +0530

    sample commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function redirect() {
+    window.location.assign("birthdaywish.html");
+}
+
+
+// function toggleMessage() {
+//     var heartCard = document.querySelector(".heart-card");
+//     heartCard.classList.toggle("clicked");
+// }
+
+function redirect1() {
+    window.location.assign("ourFriendship.html");
+}
